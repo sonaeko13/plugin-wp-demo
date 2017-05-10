ec1af8b314e4e0a58ec4d1ccfb5ae49a568cb8f8
sonaeko <sonaeko>
2017-05-10 15:24:21 +0200

    add plugin infos

---
+++ plugin-wp-demo.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Demo plugin
+Plugin URI: http://rorqal.com/
+Description: Test git pour le versionig.
+Version: 0.1
+Author: sonaeko
+Author URI: http://rorqal.com
+Text Domain: demo-plugin
+Domain Path: /lang/
+
+Copyright 2017 Rorqal.
+*/
